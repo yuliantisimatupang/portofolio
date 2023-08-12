bef0d1784cbf2bced3f594850d47c0886de9d0b9
Diyan Yagari Pratama <diyan.pratama@autopedia.id>
2023-08-12 15:20:38 +0700

    feat: add new library

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     fontFamily: {
-      sans: ['Rubik']
+      sans: ['Rubik'],
     },
     extend: {
       colors: {
-        "primary-1": "#fefcf3",
-        "primary-2": "#f3f1e4",
-        "line-dark": "#e6e3d1",
-        "black-primary": "#1c1c16",
-        "btn-primary": "#e6e4bf",
+        'primary-1': '#fefcf3',
+        'primary-2': '#f3f1e4',
+        'line-dark': '#e6e3d1',
+        'black-primary': '#1c1c16',
+        'btn-primary': '#e6e4bf',
       },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       borderWidth: {
-        '1': '1px'
-      }
+        1: '1px',
+      },
+    },
+    screens: {
+      'mobile-m': '375px',
+      'mobile-l': '425px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1440px',
+      '3xl': '1536px',
+      '4xl': '1920px',
     },
   },
   plugins: [],
-};
+}
